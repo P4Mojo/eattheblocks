3b0ce14e92c8d4b6bfa30615fec1d40e54174cb0
P4Mojo <100274133+P4Mojo@users.noreply.github.com>
2022-02-23 03:44:53 -0500

    Update Reward.sol

--- screencast/391-rewards-token/contracts/Reward.sol
+++ screencast/391-rewards-token/contracts/Reward.sol
@@ -4,7 +4,7 @@ import './yields-utils-v2/token/ERC20.sol';
 
 contract Reward is ERC20 {
   constructor() 
-  ERC20('Our Reward Token', 'RWT', 18) {
+  ERC20('LaissezFaire', 'LMZ', 18) {
     _mint(msg.sender, 1000);
   }
 }
